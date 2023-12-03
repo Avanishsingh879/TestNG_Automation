ad2413e36aafa19aad8171122c71428168d935af
unknown <avanishsingh879@gmail.com>
2023-12-04 00:10:45 +0530

    Database connect with mysql

--- src/test/java/com/genpact/stepdefinition/DB_Testing2.java
+++ src/test/java/com/genpact/stepdefinition/DB_Testing2.java
@@ -10,25 +10,26 @@ public class DB_Testing2 {
 	
 	
 	
-	//Connection with Database
+	//Connection with Database Date//3-12-2023
 	
 	public static void main(String args[]) throws ClassNotFoundException, SQLException {
 		
-		Class.forName("com.mysql.jdbc.Driver");//Driver class
+		Class.forName("com.mysql.jdbc.Driver");
+		System.out.println("Driver class Loaded");
+		Connection connection=DriverManager.getConnection("jdbc:mysql://localhost:3306/campus", "root", "Mathura@!0809");
 		
-		Connection Con = DriverManager.getConnection("jdbc:mysql://localhost:3306/campus", "root", "Mathura@!0809");
+		System.out.println("Connect with MySql");
 		
-		Statement smt=Con.createStatement();
-		
-		ResultSet rs=smt.executeQuery("select * from campus.Item");
+		Statement smt=connection.createStatement();
+		ResultSet rs=smt.executeQuery("Select * from campus.Item");
 		
 		while(rs.next()) {
 			
-			String firstname=rs.getString("FirstName");
-			System.out.println("Database Record:" +firstname);
+			String Firstname=rs.getString("FirstName");
+			
+			System.out.println("Total First Name Records " +Firstname );
 		}
 		
 		
 	}
-
 }
